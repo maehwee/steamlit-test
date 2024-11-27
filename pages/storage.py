8261e76e052d001b1769d1c6c0e23591c9b68475
maehwee <teomaehwee@gmail.com>
2024-11-27 10:34:27 -0800

    edit message page

--- pages/storage.py
+++ pages/storage.py
@@ -4,6 +4,7 @@ from st_files_connection import FilesConnection
 import pandas as pd
 import s3fs
 
+st.title("Messages)
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('s3', type=FilesConnection)
@@ -11,7 +12,7 @@ df = conn.read("streamlitbucket2/myfile.csv", input_format="csv", ttl=600)
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
+    st.write(f"{row.Name} said :{row.Message}:")
 
 
 # Writing to file
@@ -19,7 +20,7 @@ for row in df.itertuples():
 fs = s3fs.S3FileSystem(anon=False)
 
 # Streamlit app
-st.title("Message Entry")
+st.title("Send a message")
 
 # Input fields
 name = st.text_input("Enter your name")
